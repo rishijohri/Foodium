5f4fc0cda0bfc120314ec7cc4ecbe1fd451005ba
AnirudhSanghi <sanghianirudh2000@gmail.com>
2022-03-10 23:35:47 +0530

    home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,40 @@
+import NavBar from '../components/NavBar'
+import { Layout, Card, Col, Row } from 'antd';
+import React, { useState } from 'react';
+import back1 from '../images/a.jpg';
+import Slider from '../components/Slider'
+const { Header, Footer, Sider, Content } = Layout;
+const { Meta } = Card;
+var isMoibile = false
 
 
 const HomePage = () => {
 
-    return ;
+    return (
+        <Layout>
+            <Header style={{ padding: '0', marginBottom: '5vh' }}>
+                <NavBar />
+            </Header>
+            <Content>
+                <div className="site-card-wrapper">
+                    <Row gutter={16}>
+                        <Col span={10}>
+                            <Card  cover={<Slider image_array={[back1, back1]} width={'100%'} height={'50%'}/>} title="Mess 1" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={12}>  
+                            <Card   cover={<Slider image_array={[back1, back1]}  />} title="Mess2" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        
+                    </Row>
+                </div>,
+
+            </Content>
+        </Layout>
+    );
 }
 
 
